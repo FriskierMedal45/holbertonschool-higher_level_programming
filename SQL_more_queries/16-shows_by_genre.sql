3a6273c9b32f09888983cdcd3314f69842f26c5c
FriskierMedal45 <reyesesquivelm@gmail.com>
2026-01-30 10:49:02 -0600

    Update 16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,6 @@
--- Script Imports database dump
-SELECT tv_shows.title
+-- Script imports database dump
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres on tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
